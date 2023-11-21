49fbaab6329a81431d0ac16de390ea7b9f9b5341
flor3kk <127989634+flor3kk@users.noreply.github.com>
2023-11-21 19:59:06 +0100

    Update bakterieDZ.java

--- DivAndConq/bakterieDZ.java
+++ DivAndConq/bakterieDZ.java
@@ -3,15 +3,18 @@ package ALGORYTMY;
 public class bakterieDZ {
 
     public static void main(String[] args) {
-        System.out.println(bakt(6));
+        System.out.println(bakt(8));
     }
 
     static int bakt(int czas){
-        if (czas < 2){
+        if (czas == 0)
+            return 0;
+        else if (czas < 2)
             return 1;
-        }
-        else{
-            return 2*bakt(czas-2);
-        }
+        else if (czas == 2)
+            return 2 * bakt(czas-1);
+        else
+            return 2 * bakt(czas-2);
+
     }
 }
